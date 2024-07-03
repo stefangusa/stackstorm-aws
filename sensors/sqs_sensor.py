eca5d68000b6ca4c8820a75721414d4623923fb0
stefangusa <stefan.gusa.1996@gmail.com>
2024-07-03 13:25:57 +0300

    sync (#11)

--- sensors/sqs_sensor.py
+++ sensors/sqs_sensor.py
@@ -120,6 +120,9 @@ class AWSSQSSensor(PollingSensor):
         if not value and config.get('setup', None):
             value = config['setup'].get(key, None)
 
+        if value == "None":
+            value = None
+
         return value
 
     def _may_setup_sqs(self):
